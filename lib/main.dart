38e2c82c616b0a46fe5de4d8bd4237ac4809d59f
Leluth <804075486@qq.com>
2020-11-19 13:53:00 +0800

    Test Github Testop
    As mentioned in the title.

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              '你已经按了这个按钮这么多次:',
             ),
             Text(
               '$_counter',

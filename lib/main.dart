2c1c60ec64b6611aaa79283083a82695528c9119
Leluth <804075486@qq.com>
2020-11-19 19:10:20 +0800

    Revert "Test Github Testop"
    This reverts commit 38e2c82c616b0a46fe5de4d8bd4237ac4809d59f.

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     final color = Colors.white;
     _scale = 1 - _controller.value;
     return MaterialApp(
@@ -124,6 +125,42 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                   delay: delayedAmount + 5000,
                 ),
               ],
+=======
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+>>>>>>> parent of 38e2c82... Test Github Testop
             ),
           )
           //  Column(
